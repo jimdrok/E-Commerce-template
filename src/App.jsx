b85ad23ac10b03c36681e0804677ac4e630532a9
Pedro <jimdrok@gmail.com>
2025-07-11 00:31:35 -0300

    Agregar react-helmet para la gestión de metadatos en la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { Outlet } from "react-router-dom";
 import NavigationBar from "./components/Navbar/Navbar";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Tienda React con API</title>
+        <meta name="description" content="" />
+      </Helmet>
       <NavigationBar />
       <main className="main-content">
         <Outlet />
